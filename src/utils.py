866ebe0189d26028e76f590d2b51f067c2015b7f
bhuvan_10 <ramavathbhuvanachandra@gmail.com>
2026-03-31 19:05:32 +0530

    to fetch poster and plot from TMDB API

---
+++ src/utils.py
@@ -0,0 +1,21 @@
+import requests
+
+API_KEY="YOUR_TMDB_API_KEY"
+
+def fetch_poster(tmdb_id):
+    if tmdb_id!=tmdb_id:
+        return None
+
+    url=f"https://api.themoviedb.org/3/movie/{int(tmdb_id)}?api_key={API_KEY}"
+
+    try:
+        response=requests.get(url)
+        data=response.json()
+
+        poster_path=data.get("poster_path")
+        if poster_path:
+            return f"https://image.tmdb.org/t/p/w500{poster_path}"
+    except:
+        return None
+    
+    return None
