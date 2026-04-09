f699e77c7a10279ff383f4cfdab89a9485dbc8dd
Soham Sen <sohamsen1566@gmail.com>
2026-04-10 02:16:03 +0530

    update

--- src/utils.py
+++ src/utils.py
@@ -5,17 +5,13 @@ API_KEY = "39276e2761db2a5dfca927607dd76ed3"
 
 poster_cache = {}
 
-def fetch_poster(tmdb_id):
-    if pd.isna(tmdb_id):
-        return "https://via.placeholder.com/300x450?text=No+Poster%22"
-    
+PLACEHOLDER = "https://via.placeholder.com/300x450?text=No+Poster"
+
 def fetch_poster(tmdb_id):
     if pd.isna(tmdb_id):
         return PLACEHOLDER
 
     tmdb_id = int(tmdb_id)
-
-
 
     if tmdb_id in poster_cache:
         return poster_cache[tmdb_id]
@@ -24,14 +20,15 @@ def fetch_poster(tmdb_id):
 
     try:
         res = requests.get(url, timeout=3)
+
         if res.status_code != 200:
             return PLACEHOLDER
-
 
         data = res.json()
         path = data.get("poster_path")
 
         poster = f"https://image.tmdb.org/t/p/w500{path}" if path else PLACEHOLDER
+
         poster_cache[tmdb_id] = poster
         return poster
 
