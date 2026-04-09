7245d23229f3229cb9ef4a8e5ca859e286937124
Arpit <b24cs1015@iitj.ac.in>
2026-04-10 02:12:18 +0530

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -7,9 +7,15 @@ poster_cache = {}
 
 def fetch_poster(tmdb_id):
     if pd.isna(tmdb_id):
-        return "https://via.placeholder.com/300x450?text=No+Image"
+        return "https://via.placeholder.com/300x450?text=No+Poster%22"
+    
+def fetch_poster(tmdb_id):
+    if pd.isna(tmdb_id):
+        return PLACEHOLDER
 
     tmdb_id = int(tmdb_id)
+
+
 
     if tmdb_id in poster_cache:
         return poster_cache[tmdb_id]
@@ -18,20 +24,16 @@ def fetch_poster(tmdb_id):
 
     try:
         res = requests.get(url, timeout=3)
-
         if res.status_code != 200:
-            return "https://via.placeholder.com/300x450?text=No+Image"
+            return PLACEHOLDER
+
 
         data = res.json()
         path = data.get("poster_path")
 
-        if path:
-            poster = f"https://image.tmdb.org/t/p/w500{path}"
-        else:
-            poster = "https://via.placeholder.com/300x450?text=No+Image"
-
+        poster = f"https://image.tmdb.org/t/p/w500{path}" if path else PLACEHOLDER
         poster_cache[tmdb_id] = poster
         return poster
 
     except:
-        return "https://via.placeholder.com/300x450?text=No+Image"
+        return PLACEHOLDER
